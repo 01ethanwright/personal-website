a38ed9a5906da4e333adb66cfe36d9e485689136
01ethanwright <01ethanwright@gmail.com>
2022-05-15 21:24:18 -0400

    updated page css and added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { homepage } from './homepage/homepage.component';
+import { projects } from './projects/projects.component';
+import { resume } from './resume/resume.component';
+import { socials } from './socials/socials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: homepage},
+  {path: 'projects', component: projects},
+  {path: 'resume', component: resume},
+  {path: 'socials', component: socials}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { homepage } from './homepage/homepage.component';
+import { projects } from './projects/projects.component';
+import { resume } from './resume/resume.component';
+import { socials } from './socials/socials.component';
 import { navbar } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     homepage,
+    projects,
+    resume,
+    socials,
     navbar
   ],
   imports: [
