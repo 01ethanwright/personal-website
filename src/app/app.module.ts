87240d85282b4697c7249d7f95b4b888dfc3ac75
01ethanwright <01ethanwright@gmail.com>
2022-05-15 20:45:38 -0400

    Adding bootstrap and creating pages and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { homepage } from './homepage/homepage.component';
+import { navbar } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    homepage,
+    navbar
   ],
   imports: [
     BrowserModule,
